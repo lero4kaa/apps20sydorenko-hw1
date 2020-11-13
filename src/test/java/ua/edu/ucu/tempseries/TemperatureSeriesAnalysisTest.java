45c35d666340290e20420b85f8af5ef8be2dd9e0
lero4kaa <57793106+lero4kaa@users.noreply.github.com>
2020-11-13 06:26:40 +0200

    Update TemperatureSeriesAnalysisTest.java

--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -47,7 +47,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testDeviation(){
+    public void testDeviation() {
         double expResult = 3.7416573867739;
 
         double actualResult = seriesAnalysis1.deviation();
@@ -61,7 +61,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testMin(){
+    public void testMin() {
         double expResult = -5.0;
 
         double actualResult = seriesAnalysis1.min();
@@ -75,7 +75,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testMax(){
+    public void testMax() {
         double expResult = 5.0;
 
         double actualResult = seriesAnalysis1.max();
@@ -89,7 +89,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testFindTempClosestToValue(){
+    public void testFindTempClosestToValue() {
         double expResult = 3.0;
 
         double actualResult = seriesAnalysis1.findTempClosestToValue(2.5);
@@ -103,7 +103,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testFindTempClosestToZero(){
+    public void testFindTempClosestToZero() {
         double expResult = 1.0;
 
         double actualResult = seriesAnalysis2.findTempClosestToZero();
@@ -117,7 +117,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testFindTempsLessThen(){
+    public void testFindTempsLessThen() {
         double[] expResult = {-5.0, -1.0};
 
         double[] actualResult = seriesAnalysis2.findTempsLessThen(1.0);
@@ -131,7 +131,7 @@ public class TemperatureSeriesAnalysisTest {
     }
 
     @Test
-    public void testFindTempsGreaterThen(){
+    public void testFindTempsGreaterThen() {
         double[] expResult = {3.0, 1.0, 5.0};
 
         double[] actualResult = seriesAnalysis2.findTempsGreaterThen(1.0);
@@ -139,13 +139,38 @@ public class TemperatureSeriesAnalysisTest {
         assertArrayEquals(expResult, actualResult, 0.00001);
     }
 
-    @Test(expected = InputMismatchException.class)
-    public void testAddTempsWithSmallValue(){
-        int expResult = 9;
+    @Test
+    public void testAddTempsWithSmallValue() {
+        double expResult = 103.0;
+        TemperatureSeriesAnalysis seriesAnalysis3 = new TemperatureSeriesAnalysis();
 
-        int actualResult = seriesAnalysis2.addTemps(1.0, -500.00, 2.0, 3.0, 0.0, -274);
+        int actualResult = seriesAnalysis3.addTemps(1.0, -5, 2.0, 3.0, 0.0, 2.0, 100.0, 0.0);
 
         assertEquals(expResult, actualResult, 0.00001);
     }
 
+    @Test(expected = InputMismatchException.class)
+    public void testAddTempsWithSmallValueException() {
+        int actualResult = seriesAnalysis2.addTemps(1.0, -500.00, 2.0, 3.0, 0.0, -274);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSummaryStatisticsEmpty() {
+        TempSummaryStatistics statistics = seriesEmptyAnalysis.summaryStatistics();
+    }
+
+    @Test
+    public void testSummaryStatistics() {
+        TempSummaryStatistics statistics = seriesAnalysis1.summaryStatistics();
+        TempSummaryStatistics expectedStatistics =
+                new TempSummaryStatistics(1.0, 3.7416573867739, -5,5);
+        assertEquals(true, statistics.equal(expectedStatistics));
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void checkException() {
+        double[] temperatureSeriesException = {5.0, -500.0};
+        TemperatureSeriesAnalysis seriesAnalysisException =
+                new TemperatureSeriesAnalysis(temperatureSeriesException);
+    }
 }
